ce737f81ab2caa912cd592b73c49f018c2fd1145
jolver <devneonix@gmail.com>
2017-06-23 08:14:12 -0500

    first commit

---
+++ app/app.routing.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { Routes } from "@angular/router";
+
+
+const routes: Routes = [
+    //{ path: "", redirectTo: "/home", pathMatch: "full" },
+    //{ path: "home", component: ItemsComponent },
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
